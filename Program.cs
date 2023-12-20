fa69170e8ec1e673093632b20b851327404d2451
Шлома Сергей Николаевич <-@534school.spb.ru>
2023-12-20 14:35:42 +0300

    Add function string[] CreateNewMas(string[] data) for create a new array with elements that are less than 3 in length

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿//Функция для подсчета кол-ва элементов с длинной <= 3
+using System.Globalization;
+
 int CountElements(string[] data)
 {
     int count = 0;
@@ -17,7 +19,25 @@ void PrintMas(string[] mas)
     Console.WriteLine($"[{String.Join("; ", mas)}]");
 }
 
-string[] mas = {"Russia", "Denmark", "Kazan"};
+string[] CreateNewMas(string[] data)
+{
+    string[] temp = new string[CountElements(data)];
+    int j = 0;
+    for (int i = 0; i < data.Length; i++)// перебираем каждый элемент массива
+    {
+        if (data[i].Length <= 3) //Проверяем длинну строки
+        {
+            temp[j] = data[i];
+            j ++;
+        }
+    }
+    return temp;
+}
+string[] mas = {"Hello", "2", "world", ":-)"}; //Искомый массив. Варианты {"Hello", "2", "world", ":-)"} {"Russia", "Denmark", "Kazan"} {"1234", "1567", "-2", "computer science"}
+
 PrintMas(mas);
-string[] res = new string[CountElements(mas)];
+string[] res = CreateNewMas(mas); //Итоговый массив
+PrintMas(res);
+
+
 
