12a58b03e517170bb729717090c7d11ecf8fbd8b
Шлома Сергей Николаевич <-@534school.spb.ru>
2023-12-20 14:18:37 +0300

    Add function int CountElements(string[] data) for counting elements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Функция для подсчета кол-ва элементов с длинной <= 3
+int CountElements(string[] data)
+{
+    int count = 0;
+    for (int i = 0; i < data.Length; i++)// перебираем каждый элемент массива
+    {
+        if (data[i].Length <= 3) //Проверяем длинну строки
+        {
+            count ++;
+        }
+    }
+    return count; //Возвращаем кол-во элементов, удовлетворяющие условию
+}
+
+string[] mas = {"Russia", "Denmark", "Kazan"};
+string[] res = new string[CountElements(mas)];
+
